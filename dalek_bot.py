be895b5a4d5047d761058dc0a102cf4bf3e8454d
r4isstatic <r4isstatic@outlook.com>
2014-10-25 13:46:19 +0100

    Adding Dalek Bot
    Basic instructions for a couple of methods for the Dalek Bot

---
+++ dalek_bot.py
@@ -0,0 +1,16 @@
+def dalek_start(input):
+	#This method randomly shouts 'Exterminate' at any opportunity.
+	return "Exterminate!"
+
+def dalek_reply(input):
+	#If someone replies to the bot:
+
+		#if it's an insult or an attack:
+			#the Dalek should shout "You are an enemy of the Daleks! You will be exterminated!"
+
+		#if the reply just says 'exterminate!':
+			#the Dalek should shout "My vision is impaired! I cannot see!"
+
+		#or if it's just another type of reply:
+			#the Dalek shouts "Daleks conquer and destroy! Daleks conquer and destroy!"
+	return response
